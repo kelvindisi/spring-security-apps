700abd96894e122421a869fdcbdaa55634d8bfd9
Kelvin Ndisi <kelvindisi@gmail.com>
2022-09-05 16:40:05 +0300

    created module for amigoscode youtube channel security configuration project

---
+++ amigoscode-authentication/src/main/java/com/devkiu/AmigosApplication.java
@@ -0,0 +1,11 @@
+package com.devkiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmigosApplication.class, args);
+    }
+}
