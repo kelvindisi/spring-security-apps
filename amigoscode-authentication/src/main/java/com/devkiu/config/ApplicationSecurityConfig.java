88a6fd3a5e5f8a56500af90e2245ee8734c6e0c5
Kelvin Ndisi <kelvindisi@gmail.com>
2022-09-06 15:24:02 +0300

    enabled basic authentication with in memory user

--- amigoscode-authentication/src/main/java/com/devkiu/config/ApplicationSecurityConfig.java
+++ amigoscode-authentication/src/main/java/com/devkiu/config/ApplicationSecurityConfig.java
@@ -1,5 +1,50 @@
 package com.devkiu.config;
 
-public class ApplicationSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request-> {
+                    request.antMatchers("/").permitAll();
+                })
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails student = User.builder()
+                .username("kelvindisi")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(student);
+    }
 }
